9462dddc549b44bfc2cb656072d9d30381b7700e
tim s <t.sorisa@gmail.com>
2026-08-20 23:22:38 -0700

    feat: frontend component to display connection status to backend

---
+++ frontend/src/components/BackendHealthStatus.tsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+
+function BackendHealthStatus() {
+    const [status, setStatus] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:8080/health")
+            .then(response => response.json())
+            .then(data => {
+                setStatus(data.status);
+            })
+            .catch(error => {
+                console.error("Backend request failed:", error);
+                setStatus("NOT CONNECTED")
+            });
+    }, []);
+
+    return (
+        <div>
+            Backend status: {status}
+        </div>
+    );
+}
+
+export default BackendHealthStatus;
